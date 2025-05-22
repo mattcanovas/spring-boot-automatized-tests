b9a6f1bf0c5ca8c6c64a469720b0e898318e3db1
Matheus Canovas <matheuscanovas9@gmail.com>
2025-05-22 15:28:18 -0300

    refact: Implementing @BeforeEach void setup() to Person Repository Test Classe

--- src/test/java/com/github/mattcanovas/spring_boot_automatized_tests/repositories/PersonRepositoryTest.java
+++ src/test/java/com/github/mattcanovas/spring_boot_automatized_tests/repositories/PersonRepositoryTest.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -24,6 +25,13 @@ public class PersonRepositoryTest {
 
     private final PersonRepository repository;
 
+    private Person person0_;
+
+    @BeforeEach
+    public void setup() {
+        person0_ = PersonFactory.createDefaultPerson();
+    }
+
     @Autowired
     public PersonRepositoryTest(PersonRepository repository) {
         this.repository = repository;
@@ -31,19 +39,17 @@ public class PersonRepositoryTest {
 
     @Test
     public void testGivenPersonObject_WhenSave_ShouldReturnSavedPerson() {
-        Person person0 = PersonFactory.createDefaultPerson();
+        this.repository.save(person0_);
 
-        Person savedPerson = this.repository.save(person0);
-
-        assertNotEquals(savedPerson, null);
-        assertTrue(savedPerson.getId() > 0);
+        assertNotEquals(person0_, null);
+        assertTrue(person0_.getId() > 0);
     }
 
     @Test
     public void testGivenPersonList_WhenFindAll_ThenReturnPersonList() {
         this.repository.saveAll(new ArrayList<Person>() {
             {
-                add(PersonFactory.createDefaultPerson());
+                add(person0_);
                 add(new Person("Juliana", PERSON_DEFAULT_LAST_NAME, PERSON_DEFAULT_EMAIL));
             }
         });
@@ -56,13 +62,12 @@ public class PersonRepositoryTest {
 
     @Test
     public void testGivenPersonObject_AfterSaved_WhenGetReferenceById_ShouldReturnSavedPersonBefore() {
-        Person person0 = PersonFactory.createDefaultPerson();
-        person0 = this.repository.save(person0);
+        this.repository.save(this.person0_);
 
-        Person personFinded = this.repository.getReferenceById(person0.getId());
+        Person personFinded = this.repository.getReferenceById(person0_.getId());
 
         assertNotNull(personFinded);
-        assertEquals(personFinded.getId(), person0.getId());
+        assertEquals(personFinded.getId(), person0_.getId());
     }
 
     @Test
@@ -78,78 +83,73 @@ public class PersonRepositoryTest {
 
     @Test
     public void testGivenPersonObject_AfterUpdated_WhenFindById_ShoudlReturnUpdatedPersonBefore() {
-        Person person0 = PersonFactory.createDefaultPerson();
-        person0 = this.repository.save(person0);
+        this.repository.save(this.person0_);
 
-        Person personSaved = this.repository.findById(person0.getId()).get();
+        Person personSaved = this.repository.findById(person0_.getId()).get();
         personSaved.setFirstName("Rafael");
         personSaved.setEmail("validEmailForRafael@gmail.com");
 
         Person updatedPerson = this.repository.save(personSaved);
 
         assertNotNull(updatedPerson);
-        assertEquals(person0.getId(), updatedPerson.getId());
+        assertEquals(person0_.getId(), updatedPerson.getId());
         assertEquals("Rafael", updatedPerson.getFirstName());
         assertEquals("validEmailForRafael@gmail.com", updatedPerson.getEmail());
     }
 
     @Test 
     public void testGivenPersonObject_AfterSaved_WhenDeleted_ShoudlReturnNonePerson() {
-        Person person0 = PersonFactory.createDefaultPerson();
-        person0 = this.repository.save(person0);
+        this.repository.save(this.person0_);
 
-        this.repository.deleteById(person0.getId());
+        this.repository.deleteById(person0_.getId());
 
-        Optional<Person> personOptional = this.repository.findById(person0.getId());
+        Optional<Person> personOptional = this.repository.findById(person0_.getId());
 
         assertTrue(personOptional.isEmpty());
     }
 
     @Test
     public void testGivenPersonObject_AfterSaved_WhenFindByFirstNameAndLastNameWithJPQLIndexParameters_ShouldReturnSavedPersonBefore() {
-        Person person0 = PersonFactory.createDefaultPerson();
-        person0 = this.repository.save(person0);
+        this.repository.save(this.person0_);
 
-        Person personFinded = this.repository.findByFirstNameAndLastNameWithJPQLIndexesParameters(person0.getFirstName(), person0.getLastName()).get();
+        Person personFinded = this.repository.findByFirstNameAndLastNameWithJPQLIndexesParameters(person0_.getFirstName(), person0_.getLastName()).get();
 
         assertNotNull(personFinded);
-        assertEquals(personFinded.getFirstName(), person0.getFirstName());
-        assertEquals(personFinded.getLastName(), person0.getLastName());
+        assertEquals(personFinded.getFirstName(), person0_.getFirstName());
+        assertEquals(personFinded.getLastName(), person0_.getLastName());
     }
 
     @Test
     public void testGivenPersonObject_AfterSaved_WhenFindByFirstNameAndLastNameWithJPQLNamedParameters_ShouldReturnSavedPersonBefore() {
-        Person person0 = PersonFactory.createDefaultPerson();
-        person0 = this.repository.save(person0);
+        this.repository.save(person0_);
 
-        Person personFinded = this.repository.findByFirstNameAndLastNameWithJPQLNamedParameters(person0.getFirstName(), person0.getLastName()).get();
+        Person personFinded = this.repository.findByFirstNameAndLastNameWithJPQLNamedParameters(person0_.getFirstName(), person0_.getLastName()).get();
 
         assertNotNull(personFinded);
-        assertEquals(personFinded.getFirstName(), person0.getFirstName());
-        assertEquals(personFinded.getLastName(), person0.getLastName());
+        assertEquals(personFinded.getFirstName(), person0_.getFirstName());
+        assertEquals(personFinded.getLastName(), person0_.getLastName());
     }
 
     @Test
     public void testGivenPersonObject_AfterSaved_WhenFindByFirstNameAndLastNameWithNativeQueryIndexParameters_ShouldReturnSavedPersonBefore() {
-        Person person0 = PersonFactory.createDefaultPerson();
-        person0 = this.repository.save(person0);
 
-        Person personFinded = this.repository.findByFirstNameAndLastNameWithNativeQueryIndexesParameters(person0.getFirstName(), person0.getLastName()).get();
+        this.repository.save(this.person0_);
+
+        Person personFinded = this.repository.findByFirstNameAndLastNameWithNativeQueryIndexesParameters(person0_.getFirstName(), person0_.getLastName()).get();
 
         assertNotNull(personFinded);
-        assertEquals(personFinded.getFirstName(), person0.getFirstName());
-        assertEquals(personFinded.getLastName(), person0.getLastName());
+        assertEquals(personFinded.getFirstName(), person0_.getFirstName());
+        assertEquals(personFinded.getLastName(), person0_.getLastName());
     }
 
     @Test
     public void testGivenPersonObject_AfterSaved_WhenFindByFirstNameAndLastNameWithNativeQueryNamedParameters_ShouldReturnSavedPersonBefore() {
-        Person person0 = PersonFactory.createDefaultPerson();
-        person0 = this.repository.save(person0);
+        this.repository.save(person0_);
 
-        Person personFinded = this.repository.findByFirstNameAndLastNameWithNativeQueryNamedParameters(person0.getFirstName(), person0.getLastName()).get();
+        Person personFinded = this.repository.findByFirstNameAndLastNameWithNativeQueryNamedParameters(person0_.getFirstName(), person0_.getLastName()).get();
 
         assertNotNull(personFinded);
-        assertEquals(personFinded.getFirstName(), person0.getFirstName());
-        assertEquals(personFinded.getLastName(), person0.getLastName());
+        assertEquals(personFinded.getFirstName(), person0_.getFirstName());
+        assertEquals(personFinded.getLastName(), person0_.getLastName());
     }
 }
