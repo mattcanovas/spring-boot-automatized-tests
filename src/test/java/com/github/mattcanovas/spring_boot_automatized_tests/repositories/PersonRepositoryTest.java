497b6e28ad40ea903d30e984cce171ae5677110c
Matheus Canovas <matheuscanovas9@gmail.com>
2025-05-22 14:13:21 -0300

    test: Implementing PersonRepositoryTest with Save Action

--- src/test/java/com/github/mattcanovas/spring_boot_automatized_tests/repositories/PersonRepositoryTest.java
+++ src/test/java/com/github/mattcanovas/spring_boot_automatized_tests/repositories/PersonRepositoryTest.java
@@ -1,14 +1,31 @@
 package com.github.mattcanovas.spring_boot_automatized_tests.repositories;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.github.mattcanovas.spring_boot_automatized_tests.entities.Person;
 
 @DataJpaTest
 public class PersonRepositoryTest {
     
-    private final PersonRepository repository;
+    @Autowired
+    private PersonRepository repository;
 
-    public PersonRepositoryTest(PersonRepository repository) {
-        this.repository = repository;
+    @Test
+    public void testGivenPersonObject_WhenSave_ShouldReturnSavedPerson() {
+        Person person0 = new Person();
+        person0.setFirstName("Matheus");
+        person0.setLastName("Canovas");
+        person0.setEmail("test@gmail.com");
+
+        Person savedPerson = this.repository.save(person0);
+
+        assertNotEquals(savedPerson, null);
+        assertTrue(savedPerson.getId() > 0);
     }
 
 }
