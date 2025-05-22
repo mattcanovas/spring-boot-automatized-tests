244f6514102d7c7f399e89bbf5147245b5292379
Matheus Canovas <matheuscanovas9@gmail.com>
2025-05-22 13:10:32 -0300

    core: Creating project

---
+++ src/main/java/com/github/mattcanovas/sprint_boot_automatized_tests/SprintBootAutomatizedTestsApplication.java
@@ -0,0 +1,13 @@
+package com.github.mattcanovas.sprint_boot_automatized_tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootAutomatizedTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootAutomatizedTestsApplication.class, args);
+	}
+
+}
