c39bd19eee2ea68af94f1a684f0b7ae0df62c679
Matheus Canovas <matheuscanovas9@gmail.com>
2025-05-26 13:59:25 -0300

    core: Adding open api dependency and configuration

---
+++ src/main/java/configurations/OpenApiConfiguration.java
@@ -0,0 +1,26 @@
+package configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class OpenApiConfiguration {
+    
+    @Bean
+    OpenAPI customOpenApi() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("Hello Swagger OpenAPI")
+                .version("v1")
+                .description("Some description")
+                .termsOfService("https://github.com/mattcanovas")
+                .license(new License()
+                    .name("Apache 2.0")
+                    .url("https://github.com/mattcanovas")));
+    }
+
+}
